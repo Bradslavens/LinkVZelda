90fa043900465bee1702511d6679cc1e5e683aca
BradSlavens <bradslavens@gmail.com>
2022-04-11 22:55:38 -0700

    works... just needs style

--- myJs.js
+++ myJs.js
@@ -3,25 +3,27 @@ const SPAWN_RATE = 3000
 // spawn an target in a random location
 const startButton = document.getElementById("start")
 
+let intervalCounter = 0
 const gameManager = function(){
-    spawnTarget()
+    if(intervalCounter % 10 == 0) spawnTarget()
     const targets = document.getElementsByClassName("target")
     const projectiles = document.getElementsByClassName("projectile")
     compareGameObjectLocations(targets, projectiles)
         // if not move objects
     moveTarget(targets)
     moveProjectile(projectiles)
+    // console.log("loop")
+    intervalCounter++
     
 }
 
 function spawnTarget(){
-    const spawner = document.getElementById("targetSpawner")
     const target = document.createElement('div')
     target.setAttribute("class", "target")
-    let style = Math.ceil(Math.random() * 90)
+    let style = Math.ceil(Math.random() * 1000)
     // console.log(style)
-    target.style.left = `${style}%`
-    spawner.insertAdjacentElement("afterbegin", target)
+    target.style.left = `${style}px`
+    arena.insertAdjacentElement("afterbegin", target)
 }
 
 // start game 
@@ -30,7 +32,7 @@ let gameTimer = 0
 function start(){ 
     startButton.setAttribute("disabled", "")
     document.addEventListener("keydown", movePlayer)
-    gameTimer = setInterval(gameManager, 1000)    
+    gameTimer = setInterval(gameManager, 1000)
 }
 
 function stop(){ 
@@ -39,22 +41,22 @@ function stop(){
     startButton.removeAttribute("disabled")
 }
 
-function moveTarget(targets){
-    for(let i = 0; i<targets.length; i++){
-        let top = targets[i].style.top
-        const regex = /\d+/
-        let topNumber = top.match(regex)
-        let newTop = topNumber + 10
-        targets[i].style.top = newTop + "px"
-    }
-}
+// function moveTarget(targets){
+//     for(let i = 0; i<targets.length; i++){
+//         let top = targets[i].style.top
+//         const regex = /\d+/
+//         let topNumber = top.match(regex)
+//         let newTop = topNumber + 10
+//         targets[i].style.top = newTop + "px"
+//     }
+// }
 
 function moveTarget(targets){
     for(let i = 0; i<targets.length; i++){
         let top = getComputedStyle(targets[i], null).getPropertyValue("top")
         const regex = /\d+/
         let topNumber = Number(top.match(regex))
-        console.log(`topNumber ${topNumber}`)
+        // console.log(`topNumber ${topNumber}`)
         let newTop = topNumber + 10
         targets[i].style.top = newTop + "px"
     }
@@ -67,8 +69,8 @@ function moveProjectile(projectiles){
             // console.log(`projectile top ${projectileTop}`)
             const regex = /\d+/
             let nums = projectileTop.match(regex)
-            console.log(`nums ${nums}`)
-            console.log(projectiles[i])
+            // console.log(`nums ${nums}`)
+            // console.log(projectiles[i])
             projectiles[i].style.top = `${nums[0] - 20}px`
         }
     }
@@ -76,6 +78,7 @@ function moveProjectile(projectiles){
 
 // check for collision 
 function compareGameObjectLocations(targets, projectiles){
+    console.log(`proj length ${projectiles.length} targets length ${targets.length}`)
     if(projectiles.length > 0 && targets.length >0){
         for(let i = 0; i < targets.length; i++){
             for( let j = 0; j < projectiles.length; j++){
@@ -86,9 +89,10 @@ function compareGameObjectLocations(targets, projectiles){
 }
 
 function checkCollision(target = null, projectile = null) {
-    console.log(`target: ${target.offsetTop} projectile: ${projectile.offsetTop}`)
-    if(projectile.offsetTop > (target.offsetTop - 200) && projectile.offsetLeft > (target.offsetLeft - 200) && projectile.offsetLeft < (target.offsetLeft + 200)) {
-        console.log("HIT..." + target.offsetTop + " " + projectile.offsetTop )
+
+    let tt = target.offsetTop, tl = target.offsetLeft, pt = projectile.offsetTop, pl = projectile.offsetLeft
+
+    if(pt < tt + 108 && pl > tl && pl < tl + 108){
         target.remove()
         projectile.remove()
     }
@@ -131,7 +135,8 @@ function fire(){
     projectile.style.left = window.getComputedStyle(player,null).getPropertyValue("left")
 }
 
-spawnTarget()
+// fire()
+// spawnTarget()
 
 
 
